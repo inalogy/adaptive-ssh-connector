e4bb64c0160bf0416e3d0dd43beac73af99be1be
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-10 14:20:49 +0200

    test update

--- src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
+++ src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
@@ -1,6 +1,8 @@
 package com.inalogy.midpoint.connectors.ssh;
+import com.inalogy.midpoint.connectors.schema.SchemaType;
 import com.inalogy.midpoint.connectors.schema.UniversalSchemaHandler;
 import org.testng.annotations.Test;
+
 
 public class TestUniversalSchemaHandler {
     private TestProcessor testProcessor;
@@ -10,13 +12,18 @@ public class TestUniversalSchemaHandler {
     }
 
     @Test
-    public void testUniversalSchemaHandler(){
+    public void testUniversalSchemaHandler() {
         init();
-        String schemaFilePath = testProcessor.getProperties().getProperty("testSchemaFilePath");
-        String absoluteSchemaFilePath = System.getProperty("user.dir") + "/"+ schemaFilePath;
-        
-        UniversalSchemaHandler schemaHandler = new UniversalSchemaHandler(absoluteSchemaFilePath);
-        System.out.println(schemaHandler.getName() + "\n" + schemaHandler.getScriptPath());
+        String relativeSchemaFilePath = testProcessor.getConfiguration().getSchemaFilePath();
+        String absoluteSchemaFilePath = System.getProperty("user.dir") + "/"+ relativeSchemaFilePath;
+        UniversalSchemaHandler ush = new UniversalSchemaHandler(absoluteSchemaFilePath);
+        assert (ush.getSchemaTypes().size() == 2);
+        for (SchemaType schemaType: ush.getSchemaTypes()){
+            if (schemaType.getName().equals("user")){
+                assert schemaType.getScriptPath().equals("\"/home/somescript.ps2\"");
+                assert (schemaType.getAttributes().size() == 3);
+            }
+        }
     }
 
 }
