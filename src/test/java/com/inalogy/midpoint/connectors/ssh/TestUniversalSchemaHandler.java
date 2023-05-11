47b5a15eb5c8a7c96d1f7c13ab4a899405323f84
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-11 12:06:16 +0200

    added basic connector schema test

--- src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
+++ src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
@@ -2,6 +2,10 @@ package com.inalogy.midpoint.connectors.ssh;
 import com.inalogy.midpoint.connectors.schema.SchemaType;
 import com.inalogy.midpoint.connectors.schema.SchemaTypeAttribute;
 import com.inalogy.midpoint.connectors.schema.UniversalSchemaHandler;
+import org.identityconnectors.framework.common.objects.AttributeInfo;
+import org.identityconnectors.framework.common.objects.ObjectClassInfo;
+import org.identityconnectors.framework.common.objects.Schema;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.swing.plaf.synth.SynthCheckBoxMenuItemUI;
@@ -25,6 +29,24 @@ public class TestUniversalSchemaHandler {
         Map<String, SchemaType> obj = ush.getSchemaTypes();
         SchemaType  schemaTypeUser = obj.get("someUniqueName");
         assert schemaTypeUser.getCreateScript().equals("createScript.ps2");
+        System.out.println(schemaTypeUser.getAttributes().get(0).getDataType());
+    }
+    @Test
+    public void testSchema() {
+        init();
+        Schema schema = testProcessor.getConnector().schema();
+        Assert.assertNotNull(schema);
+        Assert.assertFalse(schema.getObjectClassInfo().isEmpty());
+
+        for (ObjectClassInfo objectClassInfo : schema.getObjectClassInfo()) {
+            Assert.assertFalse(objectClassInfo.getAttributeInfo().isEmpty());
+
+            if (objectClassInfo.getType().equals("user")) {
+                for (AttributeInfo attributeInfo : objectClassInfo.getAttributeInfo()) {
+                    System.out.println(attributeInfo);
+                }
+            }
+        }
     }
 
 }
