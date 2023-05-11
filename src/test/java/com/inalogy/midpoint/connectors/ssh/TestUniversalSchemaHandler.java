0c0a224c12ba15b77b25f4b13de81fbab90804e4
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-11 10:22:32 +0200

    reworked test

--- src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
+++ src/test/java/com/inalogy/midpoint/connectors/ssh/TestUniversalSchemaHandler.java
@@ -1,7 +1,12 @@
 package com.inalogy.midpoint.connectors.ssh;
 import com.inalogy.midpoint.connectors.schema.SchemaType;
+import com.inalogy.midpoint.connectors.schema.SchemaTypeAttribute;
 import com.inalogy.midpoint.connectors.schema.UniversalSchemaHandler;
 import org.testng.annotations.Test;
+
+import javax.swing.plaf.synth.SynthCheckBoxMenuItemUI;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TestUniversalSchemaHandler {
@@ -17,13 +22,9 @@ public class TestUniversalSchemaHandler {
         String relativeSchemaFilePath = testProcessor.getConfiguration().getSchemaFilePath();
         String absoluteSchemaFilePath = System.getProperty("user.dir") + "/"+ relativeSchemaFilePath;
         UniversalSchemaHandler ush = new UniversalSchemaHandler(absoluteSchemaFilePath);
-        assert (ush.getSchemaTypes().size() == 2);
-        for (SchemaType schemaType: ush.getSchemaTypes()){
-            if (schemaType.getName().equals("user")){
-                assert schemaType.getScriptPath().equals("/home/somescript.ps2");
-                assert (schemaType.getAttributes().size() == 3);
-            }
-        }
+        Map<String, SchemaType> obj = ush.getSchemaTypes();
+        SchemaType  schemaTypeUser = obj.get("someUniqueName");
+        assert schemaTypeUser.getCreateScript().equals("createScript.ps2");
     }
 
 }
