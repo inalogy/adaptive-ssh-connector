df8f83c075dd92b2779af1766681811fbd51a404
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-11 12:05:35 +0200

    added buildObjectClass method

--- src/main/java/com/inalogy/midpoint/connectors/objects/UniversalObjectsHandler.java
+++ src/main/java/com/inalogy/midpoint/connectors/objects/UniversalObjectsHandler.java
@@ -1,4 +1,28 @@
 package com.inalogy.midpoint.connectors.objects;
 
+import com.inalogy.midpoint.connectors.schema.SchemaType;
+import com.inalogy.midpoint.connectors.schema.SchemaTypeAttribute;
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.objects.AttributeInfoBuilder;
+import org.identityconnectors.framework.common.objects.ObjectClassInfoBuilder;
+import org.identityconnectors.framework.common.objects.SchemaBuilder;
+
 public class UniversalObjectsHandler {
+    private static final Log LOG = Log.getLog(SchemaType.class);
+
+
+    public static void buildObjectClass(SchemaBuilder schemaBuilder, SchemaType schemaType){
+        ObjectClassInfoBuilder objClassBuilder = new ObjectClassInfoBuilder();
+        objClassBuilder.setType(schemaType.getObjectClass()); //TODO setting type by objectClass from schema?
+        // Add attributes
+        for (SchemaTypeAttribute attribute : schemaType.getAttributes()) {
+            AttributeInfoBuilder attrInfoBuilder = new AttributeInfoBuilder(attribute.getAttributeName(), attribute.getDataType());
+            attrInfoBuilder.setRequired(attribute.isRequired());
+            attrInfoBuilder.setCreateable(attribute.isCreatable());
+            attrInfoBuilder.setUpdateable(attribute.isUpdateable());
+            attrInfoBuilder.setMultiValued(attribute.isMultivalued());
+            objClassBuilder.addAttributeInfo(attrInfoBuilder.build());
+        }
+        schemaBuilder.defineObjectClass(objClassBuilder.build());
+    }
 }
