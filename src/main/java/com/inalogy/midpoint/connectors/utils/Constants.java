8ddfbb4a32e922ded6c8c5c55dcbcab28ea23ab2
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-15 12:34:58 +0200

    first commit

---
+++ src/main/java/com/inalogy/midpoint/connectors/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.inalogy.midpoint.connectors.utils;
+
+public class Constants {
+    public static final String SEARCH_OPERATION = "SEARCH_OPERATION";
+    public static final String CREATE_OPERATION = "CREATE_OPERATION";
+    public static final String UPDATE_OPERATION = "UPDATE_OPERATION";
+    public static final String DELETE_OPERATION = "DELETE_OPERATION";
+
+    public static final String CLEAR_COMMAND_UNIX = "clear";
+    public static final String CLEAR_COMMAND_WINDOWS = "cls";
+}
