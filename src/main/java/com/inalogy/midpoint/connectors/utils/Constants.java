bec41302883a1868b7ebfb8c85afa9db48b49c83
patrikrovnak <patrik.rovnak@inalogy.com>
2023-05-30 07:52:27 +0200

    Refactor: add RESPONSE_NEW_LINE_SEPARATOR

--- src/main/java/com/inalogy/midpoint/connectors/utils/Constants.java
+++ src/main/java/com/inalogy/midpoint/connectors/utils/Constants.java
@@ -3,9 +3,8 @@ package com.inalogy.midpoint.connectors.utils;
 public class Constants {
     public static final String TYPE_POWERSHELL = "powershell";
     public static final String TYPE_SHELL = "shell";
-    public static final String CLEAR_COMMAND_UNIX = "clear";
-    public static final String CLEAR_COMMAND_WINDOWS = "cls";
-    public static final String RESPONSE_SEPARATOR = "|";
+    public static final String RESPONSE_COLUMN_SEPARATOR = "|";
+    public static final String RESPONSE_NEW_LINE_SEPARATOR = "\n";
     public static final String RESPONSE_EMPTY_ATTRIBUTE_SYMBOL = "null";
 
     public static final String SPECIAL_CONNID_NAME = "__NAME__";
@@ -18,5 +17,5 @@ public class Constants {
     public static final String MICROSOFT_EXCHANGE_ADD_UPDATEDELTA = "ADD:";
     public static final String MICROSOFT_EXCHANGE_REMOVE_UPDATEDELTA = "REMOVE:";
     public static final int SSH_RESPONSE_TIMEOUT = 30;
-    public static final int SSH_CLIENT_KEEP_ALIVE_TIMEOUT = 15;
+    public static final int SSH_CLIENT_KEEP_ALIVE_INTERVAL = 15;
 }
