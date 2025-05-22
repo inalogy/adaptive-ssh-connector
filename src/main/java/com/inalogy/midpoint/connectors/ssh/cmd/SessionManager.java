662831f36965f530d32c595bfd59c4a69f2ce4df
patrikrovnak <patrik.rovnak@inalogy.com>
2025-05-22 20:37:16 +0200

    fix: unknownUid & alreadyExists rollback

--- src/main/java/com/inalogy/midpoint/connectors/ssh/cmd/SessionManager.java
+++ src/main/java/com/inalogy/midpoint/connectors/ssh/cmd/SessionManager.java
@@ -7,7 +7,9 @@ import com.inalogy.midpoint.connectors.ssh.AdaptiveSshConfiguration;
 import com.inalogy.midpoint.connectors.ssh.utils.dynamicconfig.DynamicConfiguration;
 import com.inalogy.midpoint.connectors.ssh.utils.Constants;
 
+import com.inalogy.midpoint.connectors.ssh.utils.dynamicconfig.FlagSettings;
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.exceptions.AlreadyExistsException;
 import org.identityconnectors.framework.common.exceptions.ConnectionFailedException;
 import org.identityconnectors.framework.common.exceptions.ConnectorException;
 import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
@@ -20,6 +22,7 @@ import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.transport.verification.HostKeyVerifier;
+import org.identityconnectors.framework.common.exceptions.UnknownUidException;
 
 /**
  * This class is responsible for Initializing and establishing Ssh connection and Ssh session spawning, Execution of commands.
@@ -97,6 +100,7 @@ public class SessionManager {
 
         LOG.ok("SSH command exit status: {0}", cmd.getExitStatus());
         closeSession();
+        handleErrors(output);
         return output;
     }
 
